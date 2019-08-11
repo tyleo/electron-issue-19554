8b4d8cb094fccc7c1c5c20cce72b32ec61038bb0
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2019-08-11 06:35:02 -0700

    Remove nested folders.

--- renderer.js
+++ renderer.js
@@ -2,33 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron = require("electron");
 const fs = require("fs");
-const path = `${__dirname}/../TestFile.txt`;
+const path = `${__dirname}/TestFile.txt`;
 const run = async () => {
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
-    await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
 };
 // eslint-disable-next-line no-console
 console.log("before");
 run()
-    .then(() => {
+  .then(() => {
     // eslint-disable-next-line no-console
     console.log("then");
     electron.remote.getCurrentWebContents().toggleDevTools();
-})
-    .catch(() => {
+  })
+  .catch(() => {
     // eslint-disable-next-line no-console
     console.log("catch");
     electron.remote.getCurrentWebContents().toggleDevTools();
-})
-    // eslint-disable-next-line no-console
-    .finally(() => console.log("finally"));
+  })
+  // eslint-disable-next-line no-console
+  .finally(() => console.log("finally"));
 // eslint-disable-next-line no-console
 console.log("after");
