2293176cb44fd4e35ced9addab65bfbd8c60d874
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2019-08-11 06:57:52 -0700

    Remove eslint comments.

--- renderer.js
+++ renderer.js
@@ -4,20 +4,15 @@ const path = `${__dirname}/TestFile.txt`;
 const run = async () => {
   for (let i = 0; i < 10; ++i) await fs.promises.stat(path);
 };
-// eslint-disable-next-line no-console
 console.log("before");
 run()
   .then(() => {
-    // eslint-disable-next-line no-console
     console.log("then");
     electron.remote.getCurrentWebContents().toggleDevTools();
   })
   .catch(() => {
-    // eslint-disable-next-line no-console
     console.log("catch");
     electron.remote.getCurrentWebContents().toggleDevTools();
   })
-  // eslint-disable-next-line no-console
   .finally(() => console.log("finally"));
-// eslint-disable-next-line no-console
 console.log("after");
