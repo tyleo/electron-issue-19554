379ad118c5cd60e357140779db8370c41b49b3e3
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2019-08-11 06:14:49 -0700

    Remove local paths.

--- ts/renderer.ts
+++ ts/renderer.ts
@@ -1,56 +1,24 @@
 import * as electron from "electron";
 import * as fs from "fs";
-import * as path from "path";
+
+const path = `${__dirname}/../TestFile.txt`;
 
 const run = async () => {
-  const name = "Testa";
-  const thePath = "C:\\Users\\tyleo\\Desktop\\testa";
-  const projectFilePath = thePath + `\\${name}.json`;
-  const projectFileBody = {
-    name,
-    type: "lib",
-    srcDir: "src",
-    libDir: "lib",
-  };
-  try {
-    await fs.promises.stat(thePath);
-  } catch {
-    await fs.promises.mkdir(thePath);
-  }
-  const libDirPath = path.join(thePath, "lib");
-  const srcDirPath = path.join(thePath, "src");
-  await Promise.all([
-    fs.promises.mkdir(libDirPath),
-    fs.promises.mkdir(srcDirPath),
-  ]);
-
-  await fs.promises.writeFile(
-    projectFilePath,
-    JSON.stringify(projectFileBody, undefined, 4),
-  );
-
-  const mainFilePath = path.join(srcDirPath, "Main.node_def.json");
-  await fs.promises.writeFile(
-    mainFilePath,
-    JSON.stringify({ butt: "Hello World" }),
-  );
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
+  await fs.promises.stat(path);
 };
-
-const megaRun = async () => {
-   await run();
-   await run();
-   await run();
-   await run();
-   await run();
-   await run();
-   await run();
-   await run();
-   await run();
-}
 
 // eslint-disable-next-line no-console
 console.log("before");
-megaRun()
+run()
   .then(() => {
     // eslint-disable-next-line no-console
     console.log("then");
